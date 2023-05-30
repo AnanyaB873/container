9cf7967bc6925dfaaad03a1c7bd3f922c1eac589
Dishaga433 <ananyababu7024@gmail.com>
2023-05-30 11:57:09 +0530

    Initial commit

---
+++ lib/container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage("images/girl.jpg"),
+          fit: BoxFit.cover
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:container/container.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp()));
+
+}
